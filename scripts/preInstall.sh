7620f96580ccfc1c6a14b2462c5f1e8d9b412cf2
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-05-04 09:07:30 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -3,26 +3,3 @@
 
 echo "Waiting for software to be ready ..."
 sleep 10s;
-
-sed -i 's~ROCKETCHAT_COMMAND~> \
-      bash -c \
-        "for i in \`seq 1 30\`; do \
-          node main.js && \
-          s=$$? && break || s=$$?; \
-          echo \"Tried $$i times. Waiting 5 secs...\"; \
-          sleep 5; \
-        done; (exit $$s)"~g' ./docker-compose.yml
-
-sed -i 's~MONGO_COMMAND~mongod --oplogSize 128 --replSet rs0~g' ./docker-compose.yml
-
-sed -i 's~MONGOINIT_COMMAND~> \
-      bash -c \
-        "for i in \`seq 1 30\`; do \
-          mongo mongo/rocketchat --eval \" \
-            rs.initiate({ \
-              _id: 'rs0', \
-              members: [ { _id: 0, host: 'localhost:27017' } ]})\" && \
-          s=$$? && break || s=$$?; \
-          echo \"Tried $$i times. Waiting 5 secs...\"; \
-          sleep 5; \
-        done; (exit $$s)"~g' ./docker-compose.yml
