534a79b6a2d14094052b7a464107d338a4ca81f7
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-05-07 10:35:21 +0300

    fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,6 @@
-bash -c
-        "for i in \`seq 1 30\`; do
-          node main.js &&
-          s=$$? && break || s=$$?;
-          echo \"Tried $$i times. Waiting 5 secs...\";
-          sleep 5;
-        done; (exit $$s)"
+for i in \`seq 1 30\`; do
+  node main.js &&
+  s=$$? && break || s=$$?;
+  echo \"Tried $$i times. Waiting 5 secs...\";
+  sleep 5;
+done; (exit $$s)
