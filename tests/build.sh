13f4506f3010ffcb1bbf99a0ac37a078f277023a
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-09 11:49:11 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,2 +1,4 @@
 cp -r 6.2/* ./
-docker buildx build . --output type=docker,name=elestio4test/rocket.chat:latest | docker load
+chmox +x ./init.sh
+chmox +x ./init2.sh
+docker buildx build . --output type=docker,name=elestio4test/rocket-chat:latest | docker load
