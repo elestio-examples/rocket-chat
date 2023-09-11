9f11ee82a700fe9a479fa97d435b7c28415b1cb0
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-09-11 13:18:14 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,5 +1,5 @@
 cp -r 6.2/* ./
 chmox +x ./init.sh
 chmox +x ./init2.sh
-rm -f composer.yml
+rm -f compose.yml
 docker buildx build . --output type=docker,name=elestio4test/rocket-chat:latest | docker load
