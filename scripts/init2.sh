bbb268b61824c0797019de7c12f1c8f67fe61f26
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2024-11-26 18:40:48 +0530

    Update init2.sh

--- scripts/init2.sh
+++ scripts/init2.sh
@@ -1,9 +1,27 @@
-for i in `seq 1 30`; do
-    mongo mongo/rocketchat --eval "
-    rs.initiate({
-        _id: 'rs0',
-        members: [ { _id: 0, host: 'localhost:27017' } ]})" &&
-    s=$$? && break || s=$$?;
-    echo "Tried $$i times. Waiting 5 secs...";
-    sleep 5;
-done; (exit $$s)
+#!/bin/bash
+
+echo ====================================================
+echo ============= Initializing Replica Set =============
+echo ====================================================
+
+# Loop until MongoDB is ready to accept connections
+until mongosh --host mongo:27017 --eval 'quit(0)' &>/dev/null; do
+    echo "Waiting for mongod to start..."
+    sleep 5
+done
+
+echo "MongoDB started. Initiating Replica Set..."
+
+# Connect to the MongoDB service and initiate the replica set
+mongosh --host mongo:27017  <<EOF
+rs.initiate({
+  _id: "rs0",
+  members: [
+    { _id: 0, host: "localhost:27017" }
+  ]
+})
+EOF
+
+echo ====================================================
+echo ============= Replica Set initialized ==============
+echo ====================================================
